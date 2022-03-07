be0f5c133106f82c4927290018381cf3e0bbd10c
Vladi Bar On <vladiba@papayaglobal.com>
2022-03-07 18:32:16 +0200

    Revert "Initial commit"
    This reverts commit f756f9b9b5dac6e66598fce8e64996f83163dfec.

--- parser-exercise/src/main/java/com/example/excelparser/model/EmployeeDescriptor.java
+++
@@ -1,9 +0,0 @@
-package com.example.excelparser.model;
-
-import lombok.Data;
-
-@Data
-public class EmployeeDescriptor {
-    private String name;
-    private int id;
-}
--- parser-exercise/src/main/java/com/example/excelparser/model/EmployeeDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.excelparser.model;
-
-import lombok.Data;
-
-@Data
-public class EmployeeDto {
-    private String name;
-    private int id;
-}
